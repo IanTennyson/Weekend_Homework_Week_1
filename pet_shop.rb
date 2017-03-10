528d28ca792554171d51d9b45bb55bc1bd7e1142
Ian <iantennyson@gmail.com>
2017-03-10 17:05:58 +0000

    passed another test

--- pet_shop.rb
+++ pet_shop.rb
@@ -90,3 +90,9 @@ def customer_can_afford_pet(customer, new_pet)
   return false
 end
 
+def sell_pet_to_customer(pet_shop, pet, customer)
+  add_pet_to_customer(customer, pet)
+  increase_pets_sold(pet_shop, 1)
+  add_or_remove_cash(pet_shop, pet[:price])
+
+end
