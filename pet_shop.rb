8ee7cee43c55a95db02d53b07dfc5a0ba34abb80
Ian <iantennyson@gmail.com>
2017-03-10 15:44:04 +0000

    passed test 13

--- pet_shop.rb
+++ pet_shop.rb
@@ -59,3 +59,15 @@ def find_pet_by_name(pet_shop, pet_name)
   end
   return nil
 end
+
+def remove_pet_by_name(pet_shop, pet_name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == pet_name
+      pet_shop[:pets].delete(pet)
+    end
+  end
+end
+
+def add_pet_to_stock(pet_shop, new_pet)
+  pet_shop[:pets] << new_pet
+end
