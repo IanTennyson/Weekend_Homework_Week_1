2e6ed72f2aef11b6eb53678a202f59d5ada876c1
Ian <iantennyson@gmail.com>
2017-03-10 13:24:59 +0000

    Passed test 7

--- pet_shop.rb
+++ pet_shop.rb
@@ -12,6 +12,22 @@ def add_or_remove_cash(pet_shop, cash)
 
 end
 
+#Is that all they want?
 def pets_sold(pet_shop)
   return pet_shop[:admin][:pets_sold]
+end
+
+#Increases the number of pets sold.
+def increase_pets_sold(pet_shop, num_sold)
+  return pet_shop[:admin][:pets_sold] = num_sold
+end
+
+#How many pets in the shop: Ans 6
+def stock_count(pet_shop)
+  return pet_shop[:pets].length
+  # count = 0
+  # for stock in pet_shop[:pets]
+  #   count += 1
+  # end
+  # return count
 end
